58b43988167f0d85f6d963204d89022e62dbf168
serverbooster0101-commits <serverbooster0101@gmail.com>
2026-09-14 20:58:02 +0530

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
       {
         name: "Custom Status", // required by Discord API, not shown in the client
         state: "STALKER!!",     // this is what people actually see
-        type: 2,               // Competing
+        type: 2,               // Custom
       },
     ],
   },
