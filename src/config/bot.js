935174def58f9a104a730f2086e5be8c0500332b
serverbooster0101-commits <serverbooster0101@gmail.com>
2026-09-17 08:04:06 +0530

    Change bot activity type from 4 to 1

--- src/config/bot.js
+++ src/config/bot.js
@@ -25,7 +25,7 @@ export const botConfig = {
       {
         name: "STALKER!!", // required by Discord API, not shown in the client
         state: "STALKER.",     // this is what people actually see
-        type: 4,               // Custom
+        type: 1,               // Custom
       },
     ],
   },
